6650572be1e36bf4fe7a781410c3821db621066c
Emmanuel Adeoluwa <shegsemmanuel8@gmail.com>
2024-08-28 20:15:23 +0100

    component changes

--- src/pages/card/cart.jsx
+++ src/pages/card/cart.jsx
@@ -25,10 +25,12 @@ const Cart = () => {
         {itemsInCart.map((item, index) => {
           return (
             <div className='cart-items-title cart-items-item' key={index} id={item.id}>
+              <img src={item.image} alt="" />
               <p> {item.name} </p>
               <p> ${item.price} </p>
               <p>{cartItems[item._id]}</p>
-              <button> Remove </button>
+              <p>{item.price*cartItems[item._id]}</p>
+              <p>x</p>
             </div>
           );
         })}
