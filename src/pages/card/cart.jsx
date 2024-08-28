364254f8d8aedfbde9d6798b896d3be554c99ed8
Emmanuel Adeoluwa <shegsemmanuel8@gmail.com>
2024-08-28 22:02:14 +0100

    delivery fee & total price update

--- src/pages/card/cart.jsx
+++ src/pages/card/cart.jsx
@@ -4,7 +4,7 @@ import { food_list } from '../../assets/assets';
 import { useContext } from 'react';
 
 const Cart = () => {
-  const { cartItems } = useContext(StoreContext);
+  const { cartItems, removeFromCart } = useContext(StoreContext);
 
   // Filter food_list to get only the items that are in the cart
   const itemsInCart = food_list.filter(item => cartItems[item._id]);
@@ -29,11 +29,33 @@ const Cart = () => {
               <p> {item.name} </p>
               <p> ${item.price} </p>
               <p>{cartItems[item._id]}</p>
-              <p>{item.price*cartItems[item._id]}</p>
-              <p>x</p>
+              <p>${item.price*cartItems[item._id]}</p>
+              <p onClick={()=>removeFromCart(item._id)} className='cross'>x</p>
             </div>
           );
         })}
+      </div>
+      <div className="cart-bottom">
+          <div className="cart-total">
+              <h2>Cart Total</h2>
+              <div>
+                  <div className="cart-total-details">
+                    <p>Subtotal</p>
+                    <p>{0}</p>
+                  </div>
+                  <hr />
+                  <div className="cart-total-details">
+                    <p>Delivery Fee</p>
+                    <p>{2}</p>
+                  </div>
+                  <hr />
+                  <div className="cart-total-details">
+                    <p>Total</p>
+                    <p>{0}</p>
+                  </div>
+              </div>
+              <button>PROCEED TO CHECKOUT</button>
+          </div>
       </div>
     </div>
   );
