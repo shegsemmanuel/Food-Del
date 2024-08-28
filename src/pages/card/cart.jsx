234ce06024528df8135e1d4c222e3ec5cb50ef53
Emmanuel Adeoluwa <shegsemmanuel8@gmail.com>
2024-08-28 23:11:33 +0100

    cart section

--- src/pages/card/cart.jsx
+++ src/pages/card/cart.jsx
@@ -56,6 +56,15 @@ const Cart = () => {
               </div>
               <button>PROCEED TO CHECKOUT</button>
           </div>
+          <div className="cart-promocode">
+            <div>
+              <p>If you have a promo code, Enter it here</p>
+              <div className='cart-promocode-input'>
+                  <input type="text" placeholder='promo code' />
+                  <button>submit</button>
+              </div>
+            </div>
+          </div>
       </div>
     </div>
   );
